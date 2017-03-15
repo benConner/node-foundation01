c65152931c07aa153aa5ec94d3d4bf37346ec7ed
Ben Conner <ben.conner40@gmail.com>
2017-03-15 13:10:14 -0500

    deleted joes diceroll

--- dice-roll-testing-joeshep/lib/math.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-// Maybe should use Number(bottom) or unary, etc to make sure args are actual numbers
-const randomInt = (bottom, top) => Math.floor((Math.random() * (top - bottom + 1)) + bottom);
-
-module.exports = { randomInt }
